95a6d206dd70f7285c26fd52b1ea3f7d67f71e46
tarikcolakel <tarikcolakel@gmail.com>
2025-03-03 17:18:27 +0300

    son test hariç hepsini geçer

--- src/main/java/com/workintech/sqldmljoins/repository/KitapRepository.java
+++ src/main/java/com/workintech/sqldmljoins/repository/KitapRepository.java
@@ -9,12 +9,12 @@ import java.util.List;
 public interface KitapRepository extends JpaRepository<Kitap, Long> {
 
     //Dram ve Hikaye türündeki kitapları listeleyin. JOIN kullanmadan yapın.
-    String QUESTION_1 = "";
+    String QUESTION_1 = "SELECT k. * FROM kitap k,tur t WHERE k.turno=t.turno and t.ad in('Hikaye','Dram')";
     @Query(value = QUESTION_1, nativeQuery = true)
     List<Kitap> findBooks();
 
 
-    String QUESTION_10 = "";
+    String QUESTION_10 = "SELECT AVG(k.puan) FROM kitap k";
     @Query(value = QUESTION_10, nativeQuery = true)
     Double findAvgPointOfBooks();
 
